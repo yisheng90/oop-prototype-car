d18e7cdc8642b4b6b1ebd8d1e51e27af12de2aa7
yisheng90 <yisheng@yishenglee.com>
2016-12-17 10:39:05 +0800

    first sttempt

--- src/Car.js
+++ src/Car.js
@@ -1,22 +1,78 @@
-function Car(make, model, year, color, seats) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
+function Car (make, model, year, color, seats, passengers) {
+  this.make = make
+  this.model = model
+  this.year = year
   // TODO: add color, seats here
-
-  this.running = false;
-  this.owner = 'manufacturer';
-  this.previousOwners = [];
+  this.color = color
+  this.seats = seats
+  this.running = false
+  this.owner = 'manufacturer'
+  this.previousOwners = []
+  this.passengers = passengers || []
 }
 
-Car.prototype.sell = function(newOwner) {
-  return newOwner;
-};
+Car.prototype.sell = function (newOwner) {
+  this.previousOwners.push(this.owner)
 
-Car.prototype.paint = function(newColor) {
-  return newColor;
-};
+  this.owner = newOwner
+  return newOwner
+}
+
+Car.prototype.paint = function (newColor) {
+  this.color = newColor
+  return newColor
+}
+
+Car.prototype.start = function () {
+  this.running = true
+}
+
+Car.prototype.off = function () {
+  this.running = false
+}
+
+Car.prototype.driveTo = function (destination) {
+  if (this.running === true) {
+    console.log('driving to', destination)
+    return true
+  }
+  return false
+}
+
+Car.prototype.park = function () {
+  if (this.running !== true) {
+    console.log('parked')
+    return true
+  } else {
+    return false
+  }
+}
+
+// Part 3//
+Car.prototype.pickUp = function (name) {
+  if (this.running === true && this.passengers.length < this.seats - 1) {
+    console.log('driving to pickup', name)
+    this.passengers.push(name)
+    return true
+  }
+  return false
+}
+
+Car.prototype.dropOff = function (name) {
+  console.log()
+  if (this.running === true) {
+    if (this.passengers.includes(name)) {
+      this.passengers.splice(this.passengers.indexOf(name), 1)
+      return true
+    }
+  }
+  return false
+}
+
+Car.prototype.passengerCount = function () {
+  return this.passengers.length
+}
 
 // export the Car function for use in node //
 // this is required for the test.js to load this //
-module.exports = Car;
+module.exports = Car
